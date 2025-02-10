165429b6d3634a3a239e0eb3babfa52d9c5dc552
Meerim <kgleader@gmail.com>
2025-02-10 15:39:39 -0600

    payment option added

---
+++ lib/screens/payment_success_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class PaymentSuccessScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Оплата успешна')),
+      body: Center(
+        child: Text(
+          'Спасибо за заказ!',
+          style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
