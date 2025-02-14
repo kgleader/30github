53545256138bf5c129101ca23c19dbd811d37295
Meerim <kgleader@gmail.com>
2025-02-13 20:16:09 -0600

    cart

--- lib/widgets/menu_item_widget.dart
+++ lib/widgets/menu_item_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../models/menu_item.dart';
+import '../models/cart.dart';
 
 class MenuItemWidget extends StatelessWidget {
   final MenuItem item;
@@ -8,51 +10,56 @@ class MenuItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Tooltip(
-      message: item.description,
-      child: Card(
-        child: ListTile(
-          title: Text(item.name),
-          subtitle: Text('\$${item.price.toStringAsFixed(2)}'),
-          leading:
-              Image.asset(item.image, width: 50, height: 50, fit: BoxFit.cover),
-          onTap: () {
-            showModalBottomSheet(
-              context: context,
-              builder: (context) {
-                return Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(item.name,
-                          style: const TextStyle(
-                              fontSize: 24, fontWeight: FontWeight.bold)),
-                      const SizedBox(height: 8),
-                      Text(item.description,
-                          style: const TextStyle(
-                              fontSize: 16, color: Colors.grey)),
-                      const SizedBox(height: 8),
-                      Text("\$${item.price}",
-                          style: const TextStyle(
-                              fontSize: 22,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.green)),
-                      const SizedBox(height: 16),
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        child: const Text("Закрыть"),
-                      ),
-                    ],
-                  ),
-                );
-              },
-            );
-          },
-        ),
+    return Card(
+      child: ListTile(
+        title: Text(item.name),
+        subtitle: Text('\$${item.price.toStringAsFixed(2)}'),
+        leading:
+            Image.asset(item.image, width: 50, height: 50, fit: BoxFit.cover),
+        onTap: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (context) {
+              return Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(item.name,
+                        style: const TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.bold)),
+                    const SizedBox(height: 8),
+                    Text(item.description,
+                        style:
+                            const TextStyle(fontSize: 16, color: Colors.grey)),
+                    const SizedBox(height: 8),
+                    Text("\$${item.price}",
+                        style: const TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.green)),
+                    const SizedBox(height: 16),
+                    ElevatedButton(
+                      onPressed: () {
+                        Provider.of<Cart>(context, listen: false).addItem(item);
+                        Navigator.pop(context);
+                      },
+                      child: const Text("Добавить в корзину"),
+                    ),
+                    const SizedBox(height: 8),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text("Закрыть"),
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
