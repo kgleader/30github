63c5a685fdb3e14123931fac51ce703e77a292b2
Meerim <kgleader@gmail.com>
2025-02-22 18:59:28 -0600

    cmpt

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'models/cart.dart';
-import 'models/menu_item.dart';
 import 'screens/menu_item_screen.dart';
 
 void main() {
