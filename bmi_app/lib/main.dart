6228438d77b4fbc3749a7cfa176d70ad08d5626d
unknown <44379294+kgleader@users.noreply.github.com>
2025-03-24 23:35:01 -0600

    ds

--- bmi_app/lib/main.dart
+++ bmi_app/lib/main.dart
@@ -44,7 +44,7 @@ class _BMICalculatorScreenState extends State<BMICalculatorScreen> {
       final bmi = weight / ((height / 100) * (height / 100));
       String result;
       if (bmi < 18.5) {
-        result = "UnderWeight";
+        result = "Underweight";
       } else if (bmi < 24.9) {
         result = "Normal weight";
       } else if (bmi < 29.9) {
