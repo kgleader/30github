14ee08673bfa71693c2a15a345dee72eb65ff59d
Meerim <kgleader@gmail.com>
2025-04-25 23:51:23 -0500

    pymnt

--- menuApp/lib/screens/payment_success_screen.dart
+++ menuApp/lib/screens/payment_success_screen.dart
@@ -9,7 +9,7 @@ class PaymentSuccessScreen extends StatelessWidget {
       appBar: AppBar(title: const Text('Payment successful')),
       body: const Center(
         child: Text(
-          'Thanks for coming!',
+          'Thanks for coming to us!',
           style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
         ),
       ),
