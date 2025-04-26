80f5f58a978f62050c5b8e7df0aa0a39e37275b1
Meerim <kgleader@gmail.com>
2025-04-26 16:50:45 -0500

    menu item

--- menuApp/lib/widgets/menu_item_widget.dart
+++ menuApp/lib/widgets/menu_item_widget.dart
@@ -45,7 +45,7 @@ class MenuItemWidget extends StatelessWidget {
                         Provider.of<Cart>(context, listen: false).addItem(item);
                         Navigator.pop(context);
                       },
-                      child: const Text("Добавить в корзину"),
+                      child: const Text("Add"),
                     ),
                     const SizedBox(height: 8),
                     ElevatedButton(
