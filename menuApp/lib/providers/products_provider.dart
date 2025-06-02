f1f63d5c68521cdc150479e8b8721cd81f4867f1
Meerim <kgleader@gmail.com>
2025-06-01 23:44:43 -0500

    30git

--- menuApp/lib/providers/products_provider.dart
+++ menuApp/lib/providers/products_provider.dart
@@ -46,7 +46,7 @@ class ProductsProvider with ChangeNotifier {
   ];
 
   List<CartItem> get items {
-    return [..._items]; // ✅ Клон тизмесин кайтара
+    return [..._items]; // ✅ Клон тизмесин кайта
   }
 
   void addProduct(CartItem product) {
