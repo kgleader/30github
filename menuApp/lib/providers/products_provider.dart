85d159105a14645b052e948825fa297b2dd1bb87
Meerim <kgleader@gmail.com>
2025-10-16 13:52:34 -0500

    item 8

--- menuApp/lib/providers/products_provider.dart
+++ menuApp/lib/providers/products_provider.dart
@@ -5,7 +5,7 @@ class ProductsProvider with ChangeNotifier {
   final List<CartItem> _items = [
     CartItem(
       id: 'p1',
-      name: 'DEEP DISH PIZZA',
+      name: 'DEEP DISH PIZZA cheesy',
       price: 12.99,
       image: 'https://via.placeholder.com/100', // ✅ imageUrl'ду image кылдyk//
       category: 'Food',
