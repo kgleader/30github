22c060c6a236db3f18be1e0e33cbd884382fa6e8
Meerim <kgleader@gmail.com>
2025-11-25 20:35:59 -0600

    item 3

--- menuApp/lib/providers/products_provider.dart
+++ menuApp/lib/providers/products_provider.dart
@@ -19,7 +19,7 @@ class ProductsProvider with ChangeNotifier {
       price: 8.99,
       image: 'https://via.placeholder.com/100',
       category: 'Food',
-      title: 'Juicy Beef Burger number 1',
+      title: 'Juicy Beef Burger number 1.',
       quantity: 1,
       description: 'Classic beef burger with fresh lettuce and tomato',
     ),
